4d38a507d0fb27bae0e1db5d741fe0c48885aa44
CMichell21 <nancycarrillo200321@gmail.com>
2026-07-08 15:57:38 -0600

    Update and rename RespuestaModel.py to Respuesta.py

---
+++ app/app/src/models/Respuesta.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, ForeignKey, Integer,String
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+
+class Respuesta():
+    #esta tabla guarad las respuestas obtenidas por el sistema
+    __tablesname__="COMRES"
+    
+    Id_respuesta=Column(Integer,primary_key=True)
+    Id_peticion=Column(Integer, ForeignKey("COMPET.Id_peticion", ondelete="SET NULL"), nullable=False)
+    codigo_mensaje=Column(String(3),nullable=False, nullable=False)
+    Descripcion_mensaje=Column(String(200),nullable=False)
+    Iguales=Column(String(2),nullable=False)
+    Sugerencia=Column(String(1),nullale=True)
+    Similitud=Column(Integer, nullable=False)
+
+    
--- app/app/src/models/RespuestaModel.py
+++
@@ -1,9 +0,0 @@
-class Respuesta:
-    def __init__(self, codigoMensaje:str, descripcionMensaje:int, iguales:str, 
-                 sugerencia:str,similitud:str):
-        self.codigo=codigoMensaje
-        self.descripcion=descripcionMensaje
-        self.iguales=iguales
-        self.sugerencia=sugerencia
-        self.similitud=similitud
-    
