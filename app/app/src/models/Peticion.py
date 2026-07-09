025f609eb90fc52b924576b72e2af20da3709426
CMichell21 <nancycarrillo200321@gmail.com>
2026-07-09 03:43:19 -0600

    Update Peticion.py

--- app/app/src/models/Peticion.py
+++ app/app/src/models/Peticion.py
@@ -1,15 +1,15 @@
-from sqlalchemy import Column, ForeignKey
+from sqlalchemy import Column, ForeignKey,String,Integer
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
 
-class Peticion():
+Base=declarative_base()
+class Peticion(Base):
     #Esta tabla guarda las peticiones hechas por ek cliente
     __tablename__="COMPET"
 
-    Id_peticion=Column(int, primary_key=True)
-    Aplicacion=Column(str(20), nullable=False)
-    Id_servicio=Column(int,ForeignKey("COMSER.Id_servicio"), nullable=False)
-    Usuario=Column(str(30))
-    Terminal=Column(str(30))
-    TextoA=Column(str(32000), nullable=False)
-    TextoB=Column(str(32000), nullable=False)
-
+    Id_peticion=Column(Integer, primary_key=True)
+    Aplicacion=Column(String(20), nullable=False)
+    Id_servicio=Column(Integer,ForeignKey("COMSER.Id_servicio"), nullable=False)
+    Usuario=Column(String(30))
+    Terminal=Column(String(30))
+    TextoA=Column(String(32000), nullable=False)
+    TextoB=Column(String(32000), nullable=False)
