52aed59a9fb318ba1d2a5555dea0645693fa08b3
CMichell21 <nancycarrillo200321@gmail.com>
2026-08-03 16:19:42 -0600

    Update conexion.py

--- app/dbconexion/conexion.py
+++ app/dbconexion/conexion.py
@@ -1,39 +1,38 @@
-from app.core.cargar_propiedades import CargarPropiedades
+from app.core.except_personalizada import ErrorConexionBD
+from app.core.config import config 
 import jaydebeapi
 from app.core.logger import getLogger 
 
 class Conexion:
 
     def __init__(self):
-        self.propiedades= CargarPropiedades()
         self.logger=getLogger(__name__)
-
 
     def Conectar(self):
 
         try:
-            driver= self.propiedades.obtener_propiedades('NAME_CLASS')
-            url= self.propiedades.obtener_propiedades('URL')
-            user= self.propiedades.obtener_propiedades('DB_USER')
-            password= self.propiedades.obtener_propiedades('DB_PASSWORD')
-            addres_driver= self.propiedades.obtener_propiedades('ADDRES_DRIVER')
-
             conn= jaydebeapi.connect(
-                driver,
-                f"jdbc:{url}",
-                [user, password],
-                addres_driver
+                config.DB_DRIVER,
+                f"jdbc:{config.URL}",
+                [config.DB_USER, config.DB_PASSWORD],
+                config.DB_JAR
                 )
 
             self.logger.info("conexion exitosa")
 
             return conn
 
-        except Exception as e :
+        except Exception as error :
                                   
-            self.logger.error(f"no se pudo obtener la conexion: {e}")
-            raise
+            raise ErrorConexionBD(f"no se pudo obtener la conexion: {error}")
             
+
+            
+            
+    
+
+    
+
 
             
             
