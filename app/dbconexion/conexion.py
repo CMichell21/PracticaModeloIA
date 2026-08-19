70e78d8cb5fae9ec60b1793cd699f7b62ea41f50
CMichell21 <nancycarrillo200321@gmail.com>
2026-08-19 15:36:00 -0600

    Update conexion.py

--- app/dbconexion/conexion.py
+++ app/dbconexion/conexion.py
@@ -1,33 +1,58 @@
-from app.core.except_personalizada import ErrorConexionBD
-from app.core.config import config 
-import jaydebeapi
-from app.core.logger import getLogger 
+import pymssql
+import time
+from app.core.logger import getLogger
+from app.core.excepciones.error_conexion_bd import ErrorConexionBD
 
-class Conexion:
+class ConexionSql:
 
     def __init__(self):
         self.logger=getLogger(__name__)
 
-    def Conectar(self):
+    def conectar(self, url_ip, port, usuario, password, db, time_conection, time_read):
+
+        tiempo_inicial=time.perf_counter()
 
         try:
-            conn= jaydebeapi.connect(
-                config.DB_DRIVER,
-                f"jdbc:{config.URL}",
-                [config.DB_USER, config.DB_PASSWORD],
-                config.DB_JAR
-                )
+            self.logger.info("obteniendo conexion de la base de datos")
+
+            conn= pymssql.connect(
+                server=url_ip,
+                port=port,
+                user=usuario,
+                password=password,
+                database=db,
+                login_timeout=time_conection,
+                timeout=time_read
+            )
 
             self.logger.info("conexion exitosa")
-
-            return conn
-
-        except Exception as error :
-                                  
-            raise ErrorConexionBD(f"no se pudo obtener la conexion: {error}")
             
+            conexion=[conn,"conectar",self.__class__.__name__]
+            return conexion
 
-            
+        except Exception as error:
+            self.logger.error(f"no se pudo obtener la conexion: {error}")
+                
+            raise ErrorConexionBD("012","No se pudo realizar la operación")
+
+        finally:
+
+            tiempo_final=time.perf_counter()
+            tiempo_total=tiempo_final-tiempo_inicial
+            self.logger.info(f"tiempo maximo de conexion esperado:{time_conection}, tiempo duracion:{tiempo_total} ")
+
+
+    
+    def desconectar(self, conn):
+
+        try:
+            if conn is not None:
+                conn.close()
+                self.logger.info("conexion cerrada exitosamente")
+
+        except Exception as error:
+
+            self.logger.error(f"no se pudo cerrar la conexion{error}")
             
     
 
