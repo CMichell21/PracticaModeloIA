dc980f0c5701260a31d03d12e7baf3e1865276fb
CMichell21 <nancycarrillo200321@gmail.com>
2026-08-19 16:01:45 -0600

    Update conexion.py

--- app/dbconexion/conexion.py
+++ app/dbconexion/conexion.py
@@ -1,22 +1,47 @@
-(.venv) PS C:\Users\ncarrillo\OneDrive - FICENSA\Documents\Transformapi> python -m unittest test.dbconexion.test_conexion_sql
-E
-======================================================================
-ERROR: test_conectar (test.dbconexion.test_conexion_sql.TestConexionSql.test_conectar)
-----------------------------------------------------------------------
-Traceback (most recent call last):
-  File "C:\Users\ncarrillo\OneDrive - FICENSA\Documents\Transformapi\test\dbconexion\test_conexion_sql.py", line 17, in test_conectar
-    resultado=conexion.conectar(
-        server="localhost",
-    ...<5 lines>...
-        timeout=5
-    )
-TypeError: ConexionSql.conectar() got an unexpected keyword argument 'server'
+import unittest
+from unittest.mock import patch, MagicMock
 
-----------------------------------------------------------------------
-Ran 1 test in 0.002s
+from app... import ConexionSql
 
-FAILED (errors=1)
-(.venv) PS C:\Users\ncarrillo\OneDrive - FICENSA\Documents\Transformapi> 
+
+class TestConexionSql(unittest.TestCase):
+
+    @patch("app...conexion_sql.pymssql.connect")
+    def test_conectar(self, mock_connect):
+
+        conexion_falsa = MagicMock()
+
+        mock_connect.return_value = conexion_falsa
+
+        conexion = ConexionSql()
+
+        resultado = conexion.conectar(
+            url_ip="localhost",
+            port=1433,
+            usuario="usuario_test",
+            password="password_test",
+            db="BD_TEST",
+            time_conection=10,
+            time_read=5
+        )
+
+        self.assertEqual(resultado[0], conexion_falsa)
+        self.assertEqual(resultado[1], "conectar")
+        self.assertEqual(resultado[2], "ConexionSql")
+
+        mock_connect.assert_called_once_with(
+            server="localhost",
+            port=1433,
+            user="usuario_test",
+            password="password_test",
+            database="BD_TEST",
+            login_timeout=10,
+            timeout=5
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
     
 
 
