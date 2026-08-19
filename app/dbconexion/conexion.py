86ce62dd6dab537cd42ce99a1eda89f21572c274
CMichell21 <nancycarrillo200321@gmail.com>
2026-08-19 16:30:31 -0600

    Update conexion.py

--- app/dbconexion/conexion.py
+++ app/dbconexion/conexion.py
@@ -1,48 +1,33 @@
-import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 
-from app... import ConexionSql
+import pytest
+
+from app.infraestructura.conexion_sql import ConexionSql
+from app.core.excepciones.error_conexion_bd import ErrorConexionBD
 
 
-class TestConexionSql(unittest.TestCase):
+def test_conectar_error():
 
-    @patch("app...conexion_sql.pymssql.connect")
-    def test_conectar(self, mock_connect):
+    conexion = ConexionSql()
 
-        conexion_falsa = MagicMock()
+    with patch(
+        "app.infraestructura.conexion_sql.pymssql.connect"
+    ) as mock_connect:
 
-        mock_connect.return_value = conexion_falsa
-
-        conexion = ConexionSql()
-
-        resultado = conexion.conectar(
-            url_ip="localhost",
-            port=1433,
-            usuario="usuario_test",
-            password="password_test",
-            db="BD_TEST",
-            time_conection=10,
-            time_read=5
+        mock_connect.side_effect = Exception(
+            "Error de conexión"
         )
 
-        self.assertEqual(resultado[0], conexion_falsa)
-        self.assertEqual(resultado[1], "conectar")
-        self.assertEqual(resultado[2], "ConexionSql")
-
-        mock_connect.assert_called_once_with(
-            server="localhost",
-            port=1433,
-            user="usuario_test",
-            password="password_test",
-            database="BD_TEST",
-            login_timeout=10,
-            timeout=5
-        )
-
-
-if __name__ == "__main__":
-    unittest.main()
-    
+        with pytest.raises(ErrorConexionBD):
+            conexion.conectar(
+                url_ip="192.168.1.100",
+                port=1433,
+                usuario="usuario_test",
+                password="password_test",
+                db="BD_TEST",
+                time_conection=10,
+                time_read=20
+            )
 
 
             
